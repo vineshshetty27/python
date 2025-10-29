999e8d15720856ac7115508d9f57effa3e3aff92
vineshshetty27 <vineshshetty2004@gmail.com>
2025-10-28 23:05:04 -0700

    Add typecasting example in typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,9 @@
+num1 = 10       
+num2 = "20"     
+num3 = int(num2)       
+num4 = float(num2)  
+num2 = num2     
+print(type(num1))  
+print(type(num2))  
+print(type(num3))  
+print(type(num4))   
