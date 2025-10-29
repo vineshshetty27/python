ca07d7bb1aaf5cf7689b1a42aeee852e1be25475
vineshshetty27 <vineshshetty2004@gmail.com>
2025-10-28 23:04:30 -0700

    Create tuple list.py

---
+++ tuple list.py
@@ -0,0 +1,10 @@
+list = [10, 20, 30, 40]
+print("List:", list)
+list[1]=25
+list.append(50)         
+list.remove(30)         
+print("Modified List:", list)
+
+tuple = (10, 20, 30, 40)
+print("Tuple:", tuple)
+print("Second element:", tuple[1])
