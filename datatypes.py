ed3c976b320e32542d8d1bf312298e0a38ac5992
vineshshetty27 <vineshshetty2004@gmail.com>
2025-10-28 22:56:01 -0700

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,23 @@
+a = 10
+print(a, type(a))      
+
+b = 12.5
+print(b, type(b))      
+
+c = "Hello Python"
+print(c, type(c))      
+
+d = True
+print(d, type(d))      
+
+e = [1, 2, 3, 4]
+print(e, type(e))      
+
+f = (10, 20, 30)
+print(f, type(f))      
+
+g = {10, 20, 30, 20}
+print(g, type(g))      
+
+h = {"name": "Vinesh", "age": 20}
+print(h, type(h))      
