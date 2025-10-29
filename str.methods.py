63fbc9c821a6c4d3143c4f5485139e808ba92938
vineshshetty27 <vineshshetty2004@gmail.com>
2025-10-28 23:04:04 -0700

    Create str.methods.py

---
+++ str.methods.py
@@ -0,0 +1,12 @@
+text = "Python is Fun"
+print(text.upper())        
+print(text.lower())        
+print(text.replace("Fun", "Awesome"))  
+print(len(text))           
+print(text.startswith("Python"))  
+print(text.endswith("Fun"))       
+text2 = "python123"
+print(text2.isalpha())     
+print(text2.isdigit())     
+print(text2.isalnum())     
+print(text2.isspace())     
